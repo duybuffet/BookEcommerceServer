66c2d0a3a02cdf098f55ba4fa2716a1bdb587cd4
Tang Hoai Duy <duyth1993@gmail.com>
2015-09-30 01:32:44 +0700

    - finish admin site and improve models

--- EcommerceAPI/admin.py
+++ EcommerceAPI/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
 
+from .models import *
+
+
+class BookAuthorInline(admin.TabularInline):
+    model = BookAuthor
+    extra = 3
+
+
+class BookGenreInline(admin.TabularInline):
+    model = BookGenre
+    extra = 3
+
+
+class OrderLineInline(admin.TabularInline):
+    model = OrderLine
+    extra = 3
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = [BookAuthorInline]
+    list_display = ['name']
+    search_fields = ['name']
+
+class BookAdmin(admin.ModelAdmin):
+    inlines = [BookGenreInline, BookAuthorInline]
+    list_display = ('id', 'isbn', 'title', 'price', 'discount', 'stock')
+    search_fields = ['id', 'isbn', 'title', 'price']
+
+class OrderHistoryAdmin(admin.ModelAdmin):
+    inlines = [OrderLineInline]
+    list_display = ('id', 'fullname', 'order_date', 'status', 'total')
+    search_fields = ['id', 'fullname', 'order_date']
+    readonly_fields = ('fullname', 'order_date', 'total')
+    list_filter = ['status']
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'username', 'fullname', 'email', 'phone')
+    search_fields = ['id', 'username', 'fullname', 'email', 'phone']
+    readonly_fields = ('username', 'fullname', 'email', 'phone', 'city', 'district', 'ward', 'postal_code', 'street_number')
+
+
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+    search_fields = ['id', 'name']
+
+
 # Register your models here.
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(Genre, GenreAdmin)
+admin.site.register(OrderHistory, OrderHistoryAdmin)
--- EcommerceAPI/models.py
+++ EcommerceAPI/models.py
@@ -10,6 +10,10 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.conf import settings
+from django.utils.encoding import smart_unicode
+
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
 
 
 class Author(models.Model):
@@ -20,13 +24,17 @@ class Author(models.Model):
         managed = False
         db_table = 'tbl_author'
 
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return smart_unicode(self.name)
 
 class Book(models.Model):
     isbn = models.CharField(db_column='ISBN', unique=True, max_length=100, blank=True, null=True)  # Field name made lowercase.
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     cover_url = models.CharField(max_length=255, blank=True, null=True)
-    author = models.CharField(max_length=255, blank=True, null=True)
     price = models.FloatField()
     discount = models.FloatField(blank=True, null=True)
     stock = models.IntegerField(blank=True, null=True)
@@ -35,6 +43,15 @@ class Book(models.Model):
     class Meta:
         managed = False
         db_table = 'tbl_book'
+
+    def __str__(self):
+        if self.isbn.strip() != "":
+            return self.isbn + " - " + self.title
+        else:
+            return self.title
+
+    def __unicode__(self):
+        return smart_unicode(self.title)
 
 
 class BookAuthor(models.Model):
@@ -57,6 +74,7 @@ class BookGenre(models.Model):
 
 class Customer(models.Model):
     username = models.CharField(unique=True, max_length=255)
+    password = models.CharField(max_length=128, editable=False)
     fullname = models.CharField(max_length=255, blank=True, null=True)
     email = models.CharField(max_length=255, blank=True, null=True)
     phone = models.CharField(max_length=45, blank=True, null=True)
@@ -70,6 +88,12 @@ class Customer(models.Model):
         managed = False
         db_table = 'tbl_customer'
 
+    def __str__(self):
+        return self.username + " - " + self.fullname
+
+    def __unicode__(self):
+        return smart_unicode(self.fullname)
+
 
 class Genre(models.Model):
     name = models.CharField(max_length=255)
@@ -78,11 +102,23 @@ class Genre(models.Model):
         managed = False
         db_table = 'tbl_genre'
 
+    def __str__(self):
+        return self.name
+
+    def __unicode__(self):
+        return smart_unicode(self.name)
 
 class OrderHistory(models.Model):
-    customer = models.ForeignKey(Customer)
+    STATUS_CHOICE = (
+        (1, 'Waiting'),
+        (2, 'Confirmed And Shipping'),
+        (3, 'Completed'),
+        (4, 'Cancel')
+    )
+    customer = models.ForeignKey(Customer, editable=False)
+    staff = models.ForeignKey(AUTH_USER_MODEL)
     order_date = models.DateTimeField(blank=True, null=True)
-    status = models.SmallIntegerField(blank=True, null=True)
+    status = models.SmallIntegerField(blank=True, null=False, choices=STATUS_CHOICE, default=1)
     total = models.FloatField(blank=True, null=True)
     shipping_fee = models.FloatField(blank=True, null=True)
     shipping_address = models.TextField(blank=True, null=True)
@@ -94,6 +130,14 @@ class OrderHistory(models.Model):
         managed = False
         db_table = 'tbl_order_history'
 
+    def fullname(self):
+        return self.customer.fullname
+
+    def __str__(self):
+        return "#" + self.id + " - " + self.order_date
+
+    def __unicode__(self):
+        return smart_unicode(self.id)
 
 class OrderLine(models.Model):
     book = models.ForeignKey(Book)
